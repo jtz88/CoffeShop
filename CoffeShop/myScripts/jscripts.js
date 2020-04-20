40b2e2235b1349da0dffcd8cba6c7dddcb376ae3
JoJo <unslavedcoder@gmail.com>
2020-04-20 16:52:36 +0200

    latest changes

---
+++ CoffeShop/myScripts/jscripts.js
@@ -0,0 +1,39 @@
+﻿var button = document.getElementById('btn1');
+button.onclick = function () {
+    alert('Your item was added to your cart');
+}
+
+var button = document.getElementById('btn2');
+button.onclick = function () {
+    alert('Your item was added to your cart');
+}
+
+var button = document.getElementById('btn3');
+button.onclick = function () {
+    alert('Your item was added to your cart');
+}
+
+var button = document.getElementById('btn4');
+button.onclick = function () {
+    alert('Your item was added to your cart');
+}
+
+var button = document.getElementById('btn5');
+button.onclick = function () {
+    alert('Your item was added to your cart');
+}
+
+var button = document.getElementById('btn6');
+button.onclick = function () {
+    alert('Your item was added to your cart');
+}
+
+var button = document.getElementById('btn7');
+button.onclick = function () {
+    alert('Your booking was added to your cart');
+}
+
+var button = document.getElementById('btn8');
+button.onclick = function () {
+    alert('Your booking was added to your cart');
+}
